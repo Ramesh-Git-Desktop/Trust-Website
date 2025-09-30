0dc7ec40056bd283f278279204709223b4f17e53
Ramesh-Git-Desktop <desktopramesh8895@gmail.com>
2025-09-30 18:36:17 +0530

    commit on main.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 // index.js
-import React from 'react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './assets/CSS/style.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
+export default defineConfig({
+  plugins: [react()],
+  base: '/Trust-Website/',
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
